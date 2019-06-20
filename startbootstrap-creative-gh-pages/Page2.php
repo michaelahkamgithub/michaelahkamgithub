279953966fad4de6b4f03f0f1047f6b372c25227
michaelahkam <47873798+michaelahkam@users.noreply.github.com>
2019-06-19 20:16:46 -0400

    Maquette existant
    Modif maquette existant

---
+++ startbootstrap-creative-gh-pages/Page2.php
@@ -0,0 +1,201 @@
+<?php include "header.php" ; ?>
+
+  <!-- Masthead -->
+  <header class="masthead">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center justify-content-center text-center">
+        <div class="col-lg-10 align-self-end">
+          <h1 class="h1 TexteAccueil TexteAccueil1">AGENCE DE COMMUNICATION</h1>
+          <hr class="divider my-4">
+        </div>
+        <div class="col-lg-8 align-self-baseline">
+          <p class="text-white-75 font-weight-light mb-5 sousTitre sousTitre1">BRANDING - CONCEPTION SITE INTERNET - MARKETING</p>
+          <a class="btn btn-danger btn-xl js-scroll-trigger" href="#À propos">En savoir plus</a>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- About Section -->
+  <section class="page-section bg-primary" id="À propos">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 text-center">
+          <h2 class="text-white mt-0 quiSommesNous quiSommesNous1">Qui-sommes nous?</h2>
+          <hr class="divider light my-4">
+          <p class="text-white-50 mb-4">Book communication est une agence se spécialisant dans le domaine du graphisme, de la conception de site internet et du marketing numérique. Comme son nom l’indique elle fait allusion à l’encyclopédie de la communication. Faisant face à la croissance exponentielle de la publicité numérique qui nous inonde de part et d’autre, il est important de communiquer de manière efficace avec notre publique cible. Notre mandat consiste à accompagner, développer et soutenir les PME par la conception de l’image de marque, l’identité visuelle, la conception de site internet, la gestion des réseaux sociaux pour une meilleure visibilité tout en respectant le budget.</p>
+          <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Nos services</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Services Section -->
+  <section class="page-section" id="services">
+    <div class="container">
+      <h2 class="text-center mt-0 nosServices">Nos services</h2>
+      <hr class="divider my-4">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-gem text-primary mb-4"></i>
+            <h3 class="h4 mb-2">Image de marque</h3>
+            <p class="text-muted mb-0"></p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-laptop-code text-primary mb-4"></i>
+            <h3 class="h4 mb-2">Site internet</h3>
+            <p class="text-muted mb-0"></p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-globe text-primary mb-4"></i>
+            <h3 class="h4 mb-2">Marketing numérique</h3>
+            <p class="text-muted mb-0"></p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <i class="fas fa-4x fa-heart text-primary mb-4"></i>
+            <h3 class="h4 mb-2">Réseaux sociaux</h3>
+            <p class="text-muted mb-0"></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Portfolio Section -->
+  <section id="portfolio">
+    <div class="container-fluid p-0">
+      <div class="row no-gutters">
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/BY.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/BY.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Retouche photo
+              </div>
+              <div class="project-name">
+                Caricature
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/cb.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/cb.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Retouche photo
+              </div>
+              <div class="project-name">
+                Cyborg
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/DH.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/DH.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Retouche photo
+              </div>
+              <div class="project-name">
+                Effet déchirure
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/FS.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/FS.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Retouche photo
+              </div>
+              <div class="project-name">
+                Effet fissure
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/SF.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/SF.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Logotype
+              </div>
+              <div class="project-name">
+                Shifu
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/TC.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/TC.jpg" alt="">
+            <div class="portfolio-box-caption p-3">
+              <div class="project-category text-white-50">
+                Retouche photo
+              </div>
+              <div class="project-name">
+                Tête découpé
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Contact Section -->
+  <section class="page-section" id="contact">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 text-center">
+          <h2 class="mt-0">Contactez-nous :</h2>
+          <hr class="divider my-4">
+          <p class="text-muted mb-5">Vous avez un projet? Pourquoi attendre! Contactez-nous pour une soumission.</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+          <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
+          <div>+1(819) 580-8408</div>
+        </div>
+        <div class="col-lg-4 mr-auto text-center">
+          <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
+          <!-- Make sure to change the email address in anchor text AND the link below! -->
+          <a class="d-block" href="mailto:contact@yourwebsite.com">infobookcommunication@gmail.com</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="bg-light py-5">
+    <div class="container">
+      <div class="small text-center text-muted">Copyright &copy; 2019 - Book Communication. Tous droits réservés</div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/creative.min.js"></script>
+
+</body>
+
+</html>
