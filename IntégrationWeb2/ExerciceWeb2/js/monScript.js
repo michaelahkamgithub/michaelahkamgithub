2ce3d808080839d7146564aadcb2025c7999f17d
michaelahkam <47873798+michaelahkam@users.noreply.github.com>
2019-07-20 16:35:22 -0400

    JS
    script

---
+++ IntégrationWeb2/ExerciceWeb2/js/monScript.js
@@ -0,0 +1,10 @@
+var surface;
+var longueur;
+var largeur;
+
+longueur = Number (prompt("Veuillez entrer un nombre svp :"));
+largeur = Number (prompt("Veuillez entrer un nombre svp :"));
+
+surface = longueur * largeur;
+
+alert("La surface pour une longueur de " + largeur + " et une longueur de " + longueur + " est de " + surface + " m carre ");
