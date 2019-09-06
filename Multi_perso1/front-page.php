4822975ce0a9fb9ab66d25dc68606762e2ab9928
michaelahkam <47873798+michaelahkam@users.noreply.github.com>
2019-09-05 21:22:41 -0400

    Multi
    modif

--- Multi_perso1/front-page.php
+++ Multi_perso1/front-page.php
@@ -3,9 +3,7 @@
 <div class="slider">
     <div class="img-responsive">
         <ul class="bxslider">
-            <li><img src="<?php bloginfo('stylesheet_directory');?>/images/slider/1.jpg" alt=""/></li>
-            <li><img src="<?php bloginfo('stylesheet_directory');?>/images/slider/2.jpg" alt=""/></li>
-            <li><img src="<?php bloginfo('stylesheet_directory');?>/images/slider/3.jpg" alt=""/></li>
+            <li><img src="<?php bloginfo('stylesheet_directory');?>/images/slider/mica.jpg" alt=""/></li>
         </ul>
     </div>
 </div>
