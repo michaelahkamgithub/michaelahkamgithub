620fd10364128404516bfa4c9d4de4030b51ece9
michaelahkam <47873798+michaelahkam@users.noreply.github.com>
2019-10-04 22:34:26 -0400

    Projet final
    modif

---
+++ Projet_final1/footer.php
@@ -0,0 +1,18 @@
+<!-- Footer -->
+<footer class="bg-light py-5">
+    <div class="container">
+        <div class="small text-center text-muted">Copyright &copy; 2019 - Sylvie massé massothérapeute. Tous droits réservés</div>
+    </div>
+</footer>
+
+<script src="js/wow.min.js"></script>
+<script>
+    new WOW().init();
+</script>
+<script src="js/jquery-3.3.1.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+<script src="js/bootstrap.js"></script>
+
+<?php wp_footer(); ?>
+</body>
+</html>
